2409660608b16c68fc99253f8037420beaedc563
NikitaMashkarin <mashkarinnikita@yandex.ru>
2025-03-01 09:56:57 +0300

    Исправить check style

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -7,39 +7,39 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
-  @ExceptionHandler
-  @ResponseStatus(HttpStatus.NOT_FOUND)
-  public ErrorResponse handleNotFound(final NotFoundException e) {
-    return new ErrorResponse(e.getMessage());
-  }
-
-  @ExceptionHandler
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ErrorResponse handleNotValid(final ValidationException e) {
-    return new ErrorResponse(e.getMessage());
-  }
-
-  @ExceptionHandler
-  @ResponseStatus(HttpStatus.CONFLICT)
-  public ErrorResponse handleConflictError(final DuplicatedDataException e) {
-    return new ErrorResponse(e.getMessage());
-  }
-
-  @ExceptionHandler
-  @ResponseStatus(HttpStatus.FORBIDDEN)
-  public ErrorResponse handleAccessDenied(final AccessDeniedException e) {
-    return new ErrorResponse(e.getMessage());
-  }
-
-  public class ErrorResponse {
-    private final String error;
-
-    public ErrorResponse(String error) {
-      this.error = error;
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
-    public String getError() {
-      return error;
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleNotValid(final ValidationException e) {
+        return new ErrorResponse(e.getMessage());
     }
-  }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleConflictError(final DuplicatedDataException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ErrorResponse handleAccessDenied(final AccessDeniedException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    public class ErrorResponse {
+        private final String error;
+
+        public ErrorResponse(String error) {
+            this.error = error;
+        }
+
+        public String getError() {
+            return error;
+        }
+    }
 }
