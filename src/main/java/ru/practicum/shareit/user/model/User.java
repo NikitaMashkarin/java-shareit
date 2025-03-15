efbe649b8bb301c5fa86248c717d6d8d9c6f1fd0
NikitaMashkarin <mashkarinnikita@yandex.ru>
2025-03-15 16:53:02 +0300

    исправить checkstyle

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
