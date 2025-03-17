0bccc9b0fe221f717fc608e50f0583b9913ed109
NikitaMashkarin <mashkarinnikita@yandex.ru>
2025-03-17 15:01:31 +0300

    Исправить checkstyle

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -1,13 +1,11 @@
 package ru.practicum.shareit.user;
 
-import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
